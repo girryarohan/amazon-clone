bc6523c6ed5573e118e5a7a1f71f209166166ce1
rohanarote <rohanarote7@gmail.com>
2021-01-10 19:38:34 +0530

    Initial

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6hDp68CxPtSnitsNX6CBgHNjTVwmBsLI",
+  authDomain: "clone-863e8.firebaseapp.com",
+  projectId: "clone-863e8",
+  storageBucket: "clone-863e8.appspot.com",
+  messagingSenderId: "863443442631",
+  appId: "1:863443442631:web:55b4bff318257c067ed7e2",
+  measurementId: "G-SD8LY8JFKJ",
+};
